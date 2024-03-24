9658f6d82391597bdd54075b33fc96610920a539
hisham3 <60716742+hisham3@users.noreply.github.com>
2024-03-24 13:05:45 +0200

    Update final.sh

--- final.sh
+++ final.sh
@@ -1,6 +1,36 @@
 #!/bin/bash
 
-docker cp bd-a1:/home/eda-in-*.txt ./service-result/
-&& docker cp bd-a1:/home/k.txt ./service-result/
-&& docker cp bd-a1:/home/res_dpre.csv ./service-result/
-&& docker cp bd-a1:/home/vis.png ./service-result/
+# get container name or ID
+read -p "Container ID or Name: " CONTAINER_NAME
+
+echo "Your ContainerID is $CONTAINER_NAME ."
+
+# Define the full path to the directory inside the container
+CONTAINER_DIR='/home/doc-bd-a1'
+
+# Define the full path to the local directory where files should be copied
+LOCAL_DIR='service-result'
+
+# Check if the folder exists
+if [ ! -d $LOCAL_DIR/ ]; then
+  # Folder does not exist, so create it
+  mkdir -p $LOCAL_DIR/
+  echo "Directory $LOCAL_DIR created."
+else
+  # Folder exists
+  echo "Directory $LOCAL_DIR already exists."
+fi
+
+# Copy the output files from the container to the local machine
+docker cp $CONTAINER_NAME:$CONTAINER_DIR/res_dpre.csv $LOCAL_DIR/
+docker cp $CONTAINER_NAME:$CONTAINER_DIR/eda-in-1.txt $LOCAL_DIR/
+docker cp $CONTAINER_NAME:$CONTAINER_DIR/eda-in-2.txt $LOCAL_DIR/
+docker cp $CONTAINER_NAME:$CONTAINER_DIR/eda-in-3.txt $LOCAL_DIR/
+docker cp $CONTAINER_NAME:$CONTAINER_DIR/vis.png $LOCAL_DIR/
+docker cp $CONTAINER_NAME:$CONTAINER_DIR/k.txt $LOCAL_DIR/
+
+# Stop the container
+docker stop $CONTAINER_NAME
+
+# Give a message
+echo "Files copied and container closed."
